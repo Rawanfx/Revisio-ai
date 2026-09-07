21c63eca21e4b749550520fb2531dcfe279593b7
Rawanfx <rawanseyed580@gmail.com>
2026-09-07 18:54:22 +0300

    fix pre exam summary

--- ai-service/grpc_server/pre_exam_summary.py
+++ ai-service/grpc_server/pre_exam_summary.py
@@ -4,7 +4,7 @@ def handle_pre_exam_summary(request):
     reviews=[]
     try:
          for wt in request.weak_topics :
-                focus_point = generate_exam_summary(request.topic,request.lecture_id,request.missed_count,request.total_attempted)
+                focus_point = generate_exam_summary(wt.topic,wt.lecture_id,wt.missed_count,wt.total_attempted)
                 reviews.append(examgen_pb2.TopicReview(
                     lecture_id = wt.lecture_id,
                     topic = wt.topic,
